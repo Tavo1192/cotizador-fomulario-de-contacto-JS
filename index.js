65ad4840e7920d4711a271a7b4ea0d59771e91fd
Tavo1192 <gcastroderrico75@gmail.com>
2022-10-27 17:19:27 -0300

    agrego botones sumar y restar

--- index.js
+++ index.js
@@ -176,7 +176,7 @@ const cantidadCarrito = document.getElementById("cantidadCarrito");
 
 
 
-let carrito = [];
+let carrito = JSON.parse(localStorage.getItem("carrito"));
 
     productos.forEach((product)=> {
     let content = document.createElement("div");
@@ -221,11 +221,23 @@ let carrito = [];
     }
         console.log(carrito);
         carritoCounter();
+        saveLocal();
     });
     
 });
+
+/*----Local Storage carrito----*/
+
+const saveLocal =  () => {
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+    };
+
+ JSON.parse(localStorage.getItem("carrito"));   
+
+/*---- Termina local Storage carrito---------*/
+
 const pintarCarrito = () => {
-// verCarrito.addEventListener("click", () => {
+
     modalContainer.innerHTML = " ";
     modalContainer.style.display = "block"
     const modalHeader = document.createElement("div");
@@ -256,21 +268,37 @@ const pintarCarrito = () => {
             <img src="${product.img}">
             <h3>${product.nombre}</h3>
             <p>$${product.precio}</p>
+            <span class="restar"> ➖ </span>
             <p>Cantidad: ${product.cantidad}</p>
+            <span class="sumar"> ➕ </span>
             <p>Total: ${product.cantidad * product.precio}</p>
+            <span class="delete-product"> ❌ </span>
     
     `;
     modalContainer.append(carritoContent)
     console.log(carrito.length);
 
-    let eliminar = document.createElement("span");
-    eliminar.innerText = "❌";
-    eliminar.className = "delete-product";
-    carritoContent.append(eliminar);
+    let restar = carritoContent.querySelector(".restar")
+    restar.addEventListener("click", () => {
+        if(product.cantidad !== 1)
+      product.cantidad--; 
+      pintarCarrito();
+    });
 
-    eliminar.addEventListener("click",eliminarProducto);
+    let sumar = carritoContent.querySelector(".sumar")
+    sumar.addEventListener("click", () => {
+        product.cantidad++;
+        saveLocal();
+        pintarCarrito();
+    });
 
-   });
+    let eliminar = carritoContent.querySelector(".delete-product");
+
+    eliminar.addEventListener("click", ()=> {
+        eliminarProducto(product.id);
+    });
+
+});
    
    const total = carrito.reduce((acc, el) => acc + el.precio * el.cantidad, 0)
    
@@ -283,56 +311,34 @@ const pintarCarrito = () => {
 
 verCarrito.addEventListener("click", pintarCarrito);
 
-const eliminarProducto = () => {
-    const foundId = carrito.find((element) => element.id);
+
+const eliminarProducto = (id) => {
+    const foundId = carrito.find((element) => element.id === id);
 
     carrito = carrito.filter((carritoId) => {
         return carritoId !== foundId;
 
     });
+
     carritoCounter();
+    saveLocal(); 
     pintarCarrito();
 };
 
+
 const carritoCounter = () => {
     cantidadCarrito.style.display = "block";
-    cantidadCarrito.innerText = carrito.length;
+
+    const carritoLength = carrito.length;
+    localStorage.setItem("carritoLength", JSON.stringify(carritoLength));
+
+    cantidadCarrito.innerText = JSON.parse(localStorage.getItem("carritoLength"));
 
 };
 
+carritoCounter();
 
 
-
-
-
-
-
-// inicia llamado a json //funciona con live server
-
-// const tabla = document.querySelector("#lista-autos tbody")
-
-//     function listaAutos(){
-//        fetch('lista.json')
-       
-//             .then((response) => response.json())
-//             .then((lista) => {
-//                 lista.forEach(lista => {
-//                     const row = document.createElement("tr");
-//                     row.innerHTML += `
-//                         <td>${lista.id}</td>
-//                         <td>${lista.marca}</td>
-//                         <td>${lista.modelo}</td>
-//                         <td>${lista.version}</td>
-//                     `;
-//                     tabla.appendChild(row);
-                    
-//                 });
-                
-//             })
-//             .catch(error => console.log('Hubo un error :' + error.message))
-//     };
-    
-//     listaAutos();
 
 
 const listUsers = async () => {
